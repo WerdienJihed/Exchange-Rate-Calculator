46ac1ee2dbce03df54ab7e76424c6b8de8d15f4f
werdien jihed <informatique.jihed@gmail.com>
2022-02-20 13:50:21 +0100

    add swap currency functionality

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 "use strict";
 
 // Selecting Elements
-
+const amountElement = document.getElementById("amount");
+const currencyDropdown = document.getElementById("from");
+const foreignCurrencyDropdown = document.getElementById("to");
+const swapBtn = document.querySelector(".exchange_swap-btn");
 // Global variables
 
 // Functions
-
+const handleSwap = async () => {
+  const temp = currencyDropdown.value;
+  currencyDropdown.value = foreignCurrencyDropdown.value;
+  foreignCurrencyDropdown.value = temp;
+};
+/*  initialization */
+const dropdownInit = () => {
+  foreignCurrencyDropdown.selectedIndex = 1;
+};
+const init = async () => {
+  amountElement.value = 1;
+  dropdownInit();
+};
 // Event listeners
+swapBtn.addEventListener("click", handleSwap);
+init();
